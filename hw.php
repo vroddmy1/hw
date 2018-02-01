f60632196d422c318e2dc6e99c287768b7621fdd
vroddmy <35878859+vroddmy@users.noreply.github.com>
2018-02-01 19:22:23 +0300

    Add files via upload

---
+++ hw.php
@@ -0,0 +1,12 @@
+<?php
+	$myName = "Влад";
+	$myAge = 18;
+	$myEmail = "vroddmy@gmail.com";
+	$myCity = "Smolensk";
+	$aboutMe = "Учусь програмировать";
+?> 
+	Имя: <? echo "$myName ", "<br>";?>
+	Возраст: <? echo "$myAge", "<br>";?>
+	e-mail: <? echo "$myEmail", "<br>";?>
+	Город:  <? echo "$myCity", "<br>";?>
+	О себе: <? echo "$aboutMe","<br>";?>
